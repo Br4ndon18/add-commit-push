8a80240e80a09f4863576f2fc727b626ac67c2e6
Brandon Gutierrez <brandong@Brandons-MacBook-Air.local>
2023-11-06 01:23:28 -0600

    Update files.

---
+++ add-commit-push.py
@@ -0,0 +1,18 @@
+import os 
+import sys
+
+print ('Number of arguments:')
+print(len(sys.argv))
+print ('Argument List:')
+print(str(sys.argv))
+
+print('add-commit-push')
+print('\ngit status')
+os.system('git status')
+
+print('\ngit add -A')
+os.system('git add -A')
+print('\ngit commit -m "Update files."')
+os.system('git commit -m "Update files."')
+print('\ngit push')
+os.system('git push')
